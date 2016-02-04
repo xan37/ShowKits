3a7f38eb69a59bc83bdd6b74ea152a2c5802ae7d
Nikolay Shubenkov <n.shubenkov@be-interactive.ru>
2016-02-04 19:00:32 +0300

    Initial Commit

---
+++ ShowKits/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShowKits
+//
+//  Created by Nikolay Shubenkov on 04/02/16.
+//  Copyright © 2016 Nikolay Shubenkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
