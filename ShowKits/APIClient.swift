c95a7ff73a6911e15045ced3fefc04da971db29d
Nikolay Shubenkov <n.shubenkov@be-interactive.ru>
2016-02-04 19:01:36 +0300

    initial commit

---
+++ ShowKits/APIClient.swift
@@ -0,0 +1,31 @@
+//
+//  APIClient.swift
+//  ShowKits
+//
+//  Created by Nikolay Shubenkov on 04/02/16.
+//  Copyright © 2016 Nikolay Shubenkov. All rights reserved.
+//
+
+import UIKit
+
+/*
+parameters[@"tags"] = @"cats";
+parameters[@"bbox"] = @"bbox";
+parameters[@"lat"]  = @(self.mapView.centerCoordinate.latitude);
+parameters[@"lon"]  = @(self.mapView.centerCoordinate.longitude);
+parameters[@"radius"] = @"5";
+parameters[@"extras"] = @"url_l,geo,date_taken,owner_name";
+parameters[@"format"] = @"json";
+
+
+parameters[@"content_type"] = @1;
+parameters[@"nojsoncallback"] = @1;
+
+parameters[@"method"] = @"flickr.photos.search";
+
+parameters[@"api_key"] = @"2b2c9f8abc28afe8d7749aee246d951c";
+*/
+
+class APIClient: NSObject {
+
+}
